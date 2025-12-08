d82d4e0a9f84c090b389f1fde7bffb8321feac51
Giovanna Santos Marciano <giova@DBC-001554.dbccompany.com.br>
2025-12-08 18:17:03 -0300

    front

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL;
+
+// tira / no final, se tiver, e adiciona /api
+const normalizedBaseURL = baseURL.replace(/\/+$/, "");
+
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${normalizedBaseURL}/api`,
 });
 
-//interceptador de token
+// interceptador de token
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if(token){
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
